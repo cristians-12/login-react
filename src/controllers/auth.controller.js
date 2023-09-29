f80e0a4a29250b51856b3ef72de5e33ef60ff8a8
Eggward12 <cristian.sanchez.casd@gmail.com>
2023-09-28 22:53:39 -0500

    min 50

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,48 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+    const passhash = await bcrypt.hash(password,10)
+
     const newUser = new User({
       username,
       email,
-      password,
+      password:passhash,
     });
 
-    await newUser.save();
+    const userSaved = await newUser.save();
+
+    jwt.sign({
+      id:userSaved._id,
+
+    },
+      'secret123',
+      {expiresIn: "1d"},
+      (err, token)=>{
+        if (err) console.log(err);
+        res.cookie('token',token);
+        res.json({
+          message: 'Usuario creado exitosamente'
+        })
+      }
+    )
+
+    // res.json({
+    //   id: userSaved._id,
+    //   username: userSaved.username,
+    //   email: userSaved.email,
+    //   createdAt: userSaved.createdAt,
+    //   updatedAt: userSaved.updatedAt
+    // }
+    // )
   } catch (error) {
     console.log(error);
   }
 
-  res.send("Registrando");
 };
 
 export const login = (req, res) => res.send("login");
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import app from './app.js'
 import {connectDB} from './db.js'
 
 
-
 connectDB();
 app.listen(3000)
 console.log("server on port",3000)
