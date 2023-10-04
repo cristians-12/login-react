840fd4846bfad3a32833ab382ee782271ed9ef3e
Eggward12 <cristian.sanchez.casd@gmail.com>
2023-10-04 00:18:40 -0500

    1.27

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,22 @@
+import Task from "../models/task.model.js";
+
+export const getTasks = async (req, res) => {
+    const tasks = await Task.find()
+    res.json(tasks)
+};
+
+export const crearTask = async (req, res) => {
+    const {title, description, date} = req.body;
+    const newTask = new Task({
+        title,
+        description,
+        date
+    })
+    const savedTask = await newTask.save();
+    res.json(savedTask);
+};
+export const getTask = async (req, res) => {};
+export const updateTask = async (req, res) => {};
+export const borrarTask = async (req, res) => {};
+
+
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { authRequerida } from "../middlewares/validarToken.js";
+import {
+  borrarTask,
+  crearTask,
+  getTask,
+  getTasks,
+  updateTask,
+} from "../controllers/tasks.controller.js";
 
-const router = Router()
-router.get('/tasks', authRequerida, (req,res)=>{
-    res.send("Enviando tarea.")
-})
+const router = Router();
+
+router.get("/tasks", authRequerida, getTasks);
+router.post("/tasks", authRequerida, crearTask);
+router.delete("/tasks/:id", authRequerida, borrarTask);
+router.get("/tasks/:id", authRequerida, getTask);
+router.put("/tasks/:id", authRequerida, updateTask);
 
 export default router;
